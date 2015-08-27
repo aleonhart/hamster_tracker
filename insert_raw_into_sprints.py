22c80eb4f050453e526d69c9fd41f5154c91e60a
Michelle Leonhart <michelle@tinwhiskers.net>
2015-08-26 21:09:50 -0700

    doing insert after check

--- insert_raw_into_sprints.py
+++ insert_raw_into_sprints.py
@@ -47,19 +47,21 @@ for raw_data_file in os.listdir('/home/pi/Desktop/hamster_tracker/data'):
             print "SELECT * FROM sprints WHERE start_datetime = '{}'".format(sprint_start_time)
             curs.execute("SELECT * FROM sprints WHERE start_datetime = '{}'".format(sprint_start_time))
             data = curs.fetchall()
-            print data
-            
-            # print "INSERT INTO sprints (start_datetime, end_datetime, rotations) " \
-            #       "VALUES ({}, {}, {})".format(sprint_start_time, sprint_end_time, rotations)
 
-            # curs.execute("INSERT INTO sprints (start_datetime, end_datetime, rotations) VALUES ({}, {}, {})".format())
+            print data
+            if not data:
+                print "INSERT INTO sprints (start_datetime, end_datetime, rotations) " \
+                      "VALUES ({}, {}, {})".format(sprint_start_time, sprint_end_time, rotations)
+
+                # curs.execute("INSERT INTO sprints (start_datetime, end_datetime, rotations) VALUES ({}, {}, {})".format())
             conn.commit()
             # os.rename(data_dir + raw_data_file, processed_data_dir + raw_data_file)
             time.sleep(1)
 
         except Exception as e:
             print e
-            conn.close()
+            if conn:
+                conn.close()
 
 conn.close()
 
