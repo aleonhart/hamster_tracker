c557451464328a7b07d1f3b1eb8b8ba80e8a0fcc
Alex Leonhart <alexleonhartcode@gmail.com>
2019-11-27 19:58:23 -0800

    Cleaning up scripts to be easier to read

--- collect_data.py
+++ collect_data.py
@@ -1,76 +1,87 @@
 #!/usr/bin/env python
 
 """
-Listens to sensor input on PIN 18 and logs
-the current time upon signal.
+Collects data from the sensor attached to the Raspberry Pi and saves that data to files.
 """
 
-
-import RPi.GPIO as GPIO
-from datetime import datetime, timedelta
 import time
 import os
+from datetime import datetime, timedelta
+import RPi.GPIO as GPIO
 
 
-led_pin = 7
-hall_effect_sensor_pin = 18
+# Raspberry Pi Pins
+led_pin = 7      # The Raspberry Pi pin you plug your LED into
+sensor_pin = 18  # The Raspberry Pi pin you plug the Hall Effect (HE) sensor into
 
-is_hall_effect_sensor_on = False
-is_sprint = False
-
-in_progress_data_dir = "data/in_progress_data/"
-data_dir = "data/"
-
+# Raspberry Pi Setup
 GPIO.setmode(GPIO.BOARD)
-
 GPIO.setup(led_pin, GPIO.OUT)                # set LED pin to OUTPUT
-GPIO.setup(hall_effect_sensor_pin, GPIO.IN)  # set HALL pin to INPUT
+GPIO.setup(sensor_pin, GPIO.IN)  # set sensor pin to INPUT
 
-previous_state = 1
+# Data Directories
+data_dir_live = "data/1_data_live/"    # Files that are currently being written to
+data_dir_ready = "data/2_data_ready/"  # Files that are ready to be handled by send_data.py
+
+# HE sensor is HIGH normally and LOW if the magnet is present
+MAGNET_PRESENT = 0
+MAGNET_NOT_PRESENT = 1
+
+# Initialize States
+previous_sensor_state = MAGNET_NOT_PRESENT
+is_magnet_present = False
+sprint_is_active = False
 time_of_last_recording = datetime.now()
-current_time_str = time.strftime("%Y%m%d-%H%M%S")
 current_file_name = ''
 
+# This script is meant to be left running
 while True:
     try:
-        if is_sprint == True and datetime.now() - time_of_last_recording > timedelta(seconds=10):
+        # If we're mid-sprint and the hamster is still for >10 seconds, close the sprint. The hamster has left the wheel.
+        if sprint_is_active and (datetime.now() - time_of_last_recording) > timedelta(seconds=10):
             print("Sprint Ending. Closing file.")
-            is_sprint = False
-            os.rename(in_progress_data_dir + current_file_name, data_dir + current_file_name)
-        
-        # HE sensor is HIGH normally and LOW if magnet
-        is_hall_effect_sensor_on = GPIO.input(hall_effect_sensor_pin)
-        if is_hall_effect_sensor_on != previous_state:
-            if is_hall_effect_sensor_on == 0:
-                if not is_sprint:
-                    is_sprint = True
-                    current_time_str = time.strftime("%Y%m%d-%H%M%S")
-                    current_file_name = 'raw_data_' + current_time_str
-                    raw_data_file = open(in_progress_data_dir + current_file_name, 'wb')
-                    print("Sprint Starting. Opening file: %s" % raw_data_file.name)
+            sprint_is_active = False
+            # Move the finished sprint file to the "ready" directory
+            os.rename(data_dir_live + current_file_name, data_dir_ready + current_file_name)
 
+        # If the hamster starts running, we should sense the magnet passing by
+        is_magnet_present = GPIO.input(sensor_pin)
+        if is_magnet_present != previous_sensor_state:
+            if is_magnet_present == MAGNET_PRESENT:
+                # If we don't have an active sprint, we need to start one and create a new file
+                if not sprint_is_active:
+                    sprint_is_active = True
+                    current_file_name = 'raw_data_{}'.format(time.strftime("%Y%m%d-%H%M%S"))
+                    data_file = open(data_dir_live + current_file_name, 'wb')
+                    print("Sprint Starting. Opening file: {}".format(data_file.name))
+
+                # If we do have an active sprint, we can write to the existing sprint file
                 print(time.strftime("%Y-%m-%d %H:%M:%S"))
-                raw_data_file.write(time.strftime("%Y-%m-%d %H:%M:%S"))
-                raw_data_file.write("\n")
-                GPIO.output(led_pin, True)
+                data_file.write(time.strftime("%Y-%m-%d %H:%M:%S"))
+                data_file.write("\n")
                 time_of_last_recording = datetime.now()
 
-            previous_state = is_hall_effect_sensor_on
+                # Turn on the LED for debugging
+                GPIO.output(led_pin, True)
 
+            # Take note of whether the magnet is here or not
+            previous_sensor_state = is_magnet_present
+
+            # Turn off the LED for debugging
             GPIO.output(led_pin, False)
 
     except KeyboardInterrupt:
-        GPIO.output(led_pin, False)
-        GPIO.cleanup()
-        if not raw_data_file.closed:
-            raw_data_file.close()
+        print("Script stopped!")
 
+
+print("Cleaning up")
 GPIO.output(led_pin, False)
 GPIO.cleanup()
-if not raw_data_file.closed:
-    raw_data_file.close()
+if not data_file.closed:
+    print("Closing the file.")
+    data_file.close()
 
-os.rename(in_progress_data_dir + current_file_name, data_dir + current_file_name)
+os.rename(data_dir_live + current_file_name, data_dir_ready + current_file_name)
 
 
 
--- send_data_to_thingspeak.py
+++ send_data_to_thingspeak.py
@@ -1,69 +1,61 @@
 """
-Processes data and uploads it to ThingSpeak.
-
-Intended to be run as a cron job every X minutes.
-Looks for all files in raw_data_dir, and moves them
-to processed_data_dir after processing.
+Takes data available in files and sends it to ThingSpeak
 """
 
 
-import httplib
-import urllib
+import requests
 import os
 import time
 
 
-api_key = ''
-data_dir = '/home/pi/Desktop/The_Hamstrometer/data/'
-processed_data_dir = '/home/pi/Desktop/The_Hamstrometer/data/processed_files/'
-WHEEL_CIRCUMFERENCE_INCHES = 18.0
+# ThingSpeak Data
+thingspeak_api_key = ''  # Put your ThingSpeak API key here
+thingspeak_headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
+
+# Data Directories
+data_dir_ready = '/home/pi/Desktop/The_Hamstrometer/data/2_data_ready/'
+data_dir_processed = '/home/pi/Desktop/The_Hamstrometer/data/3_data_processed/'
+
+# Calculation Constants
+WHEEL_CIRCUMFERENCE_INCHES = 18.0  # Put your wheel circumference here
 INCHES_PER_FOOT = 12.0
 FEET_PER_MILE = 5280.0
 
-# omega = rotations per second
-# kinetic energy = 0.5 * mass * (radius * omega)^2
-#                = 0.5 * 0.025g * (0.075m * omega)^2
+# Process all files in the "ready" directory
+for data_file in os.listdir(data_dir_ready):
+    if data_file.startswith('raw_'):
 
-# TODO calculate rotations per second for each sprint
-# omega = rotations / (end_time - start_time)
-
-
-
-for raw_data_file in os.listdir(data_dir):
-    if raw_data_file.startswith('raw_'):
+        # Count the number of wheel rotations in the sprint
         sprint_start_time = None
-        with open(data_dir + raw_data_file) as f:
+        with open(data_dir_ready + data_file) as f:
             rotations = 0 
             for line in f:
                 if not sprint_start_time:
                     sprint_start_time = line[:-1]
-                    print sprint_start_time
                 rotations = rotations + 1
 
+        # Calculate how far those wheel rotations translate to
         human_feet = rotations * WHEEL_CIRCUMFERENCE_INCHES / INCHES_PER_FOOT
         human_miles = human_feet / FEET_PER_MILE
-        params = urllib.urlencode({
-                                   'field1': rotations,
-                                   'field2': human_feet,
-                                   'field3': human_miles,
-                                   'created_at': sprint_start_time,
-                                   'timezone': 'America/Los_Angeles',
-                                   'key': api_key
-                                   })
-        headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
-        conn = httplib.HTTPConnection("api.thingspeak.com:80")
-        try:
-            conn.request("POST", "/update", params, headers)
-            response = conn.getresponse()
-            print("params:")
-            print params
-            print response.status, response.reason
-            conn.close()
-            os.rename(data_dir + raw_data_file, processed_data_dir + raw_data_file)
-            time.sleep(15)
 
-        except Exception as e:
-            print e
-    
+        # Send the data to ThingSpeak
+        request_data = {
+           'field1': rotations,
+           'field2': human_feet,
+           'field3': human_miles,
+           'created_at': sprint_start_time,
+           'timezone': 'America/Los_Angeles',
+           'key': thingspeak_api_key
+        }
 
+        print("Sending to ThingSpeak: Params {}".format(request_data))
+        r = requests.post('https://api.thingspeak.com/update', data=request_data, headers=thingspeak_headers)
 
+        if r.status_code == 200:
+            # Move finished files to the "processed" directory
+            os.rename(data_dir_ready + data_file, data_dir_processed + data_file)
+        else:
+            print("Failed to send data to ThingSpeak: {} {}".format(r.status_code, r.text))
+
+        # Prevent us from hitting ThingSpeak's throttles
+        time.sleep(15)
